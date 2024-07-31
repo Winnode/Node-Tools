15e67314fb29afef6b20f2a3dc347e96795d4262
WinNode <maladarsih@gmail.com>
2024-07-31 09:13:17 +0700

    Update Empeirias.sh

--- Empeirias.sh
+++ Empeirias.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-HIJAU='\033[0;32m'
-KUNING='\033[1;33m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
-NC='\033[0m'
+NC='\033[0m' 
 
-echo -e "${KUNING}=================================================================="
-echo -e "${CYAN}           INSTALASI NODE WINSNIP TOOLS EMPEIRIAS AUTO             "
-echo -e "${KUNING}==================================================================${NC}"
+echo -e "${YELLOW}=================================================================="
+echo -e "${CYAN}            WINSNIP TOOLS NODE INSTALL EMPEIRIAS AUTO             "
+echo -e "${YELLOW}==================================================================${NC}"
 
-read -p "Masukkan nama WALLET: " WALLET
+read -p "Enter WALLET name: " WALLET
 echo 'export WALLET='$WALLET
-read -p "Masukkan MONIKER Anda: " MONIKER
+read -p "Enter your MONIKER: " MONIKER
 echo 'export MONIKER='$MONIKER
-read -p "Masukkan PORT Anda (misalnya 17, port default=26): " PORT
+read -p "Enter your PORT (for example 17, default port=26): " PORT
 echo 'export PORT='$PORT
 
 echo "export WALLET=$WALLET" >> $HOME/.bash_profile
@@ -25,12 +25,12 @@ source $HOME/.bash_profile
 printLine
 echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
 echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
-echo -e "Chain ID:       \e[1m\e[32m$EMPED_CHAIN_ID\e[0m"
-echo -e "Port kustom node:  \e[1m\e[32m$EMPED_PORT\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$EMPED_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$EMPED_PORT\e[0m"
 printLine
 sleep 1
 
-printGreen "1. Menginstal Go..." && sleep 1
+printGreen "1. Installing Go..." && sleep 1
 cd $HOME
 VER="1.22.3"
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
@@ -45,13 +45,13 @@ echo $(go version) && sleep 1
 
 source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
 
-printGreen "4. Menginstal binary..." && sleep 1
+printGreen "4. Installing binary..." && sleep 1
 cd $HOME
 curl -LO https://github.com/empe-io/empe-chain-releases/raw/master/v0.1.0/emped_linux_amd64.tar.gz
 tar -xvf emped_linux_amd64.tar.gz 
 mv emped ~/go/bin
 
-printGreen "5. Mengonfigurasi dan menginisialisasi aplikasi..." && sleep 1
+printGreen "5. Configuring and init app..." && sleep 1
 emped config node tcp://localhost:${EMPED_PORT}657
 emped config keyring-backend os
 emped config chain-id empe-testnet-2
@@ -59,13 +59,13 @@ emped init $MONIKER --chain-id empe-testnet-2
 sleep 1
 echo done
 
-printGreen "6. Mengunduh genesis dan addrbook..." && sleep 1
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
 wget -O $HOME/.empe-chain/config/genesis.json https://server-5.itrocket.net/testnet/empeiria/genesis.json
-wget -O $HOME/.empe-chain/config/addrbook.json https://server-5.itrocket.net/testnet/empeiria/addrbook.json
+wget -O $HOME/.empe-chain/config/addrbook.json  https://server-5.itrocket.net/testnet/empeiria/addrbook.json
 sleep 1
 echo done
 
-printGreen "7. Menambahkan seeds, peers, konfigurasi port kustom, pruning, harga gas minimum..." && sleep 1
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
 SEEDS="20ca5fc4882e6f975ad02d106da8af9c4a5ac6de@empeiria-testnet-seed.itrocket.net:28656"
 PEERS="03aa072f917ed1b79a14ea2cc660bc3bac787e82@empeiria-testnet-peer.itrocket.net:28656,004e2924efb660169e27d55518909b24f902dd48@155.133.27.170:26656,7f777a33fc94dfdade513c161a0bafbb0cfc2025@213.199.45.86:43656,5faa12744223fd0aea91970e405d69731ff35fed@62.169.17.9:43656,33cfcfa07ad55331d40fb7bcda010b0156328647@149.102.144.171:43656,3e30e4b87bdd45e9715b0bbf02c9930d820a3158@164.132.168.149:26656,bb15883943a2f31b1ca73247a1b0526a5778f23a@135.181.94.81:26656,e058f20874c7ddf7d8dc8a6200ff6c7ee66098ba@65.109.93.124:29056,0340080d68f88eb6944bd79c86abd3c9794eb0a0@65.108.233.73:13656,45bdc8628385d34afc271206ac629b07675cd614@65.21.202.124:25656,a9cf0ffdef421d1f4f4a3e1573800f4ee6529773@136.243.13.36:29056,878d0e8b9741adc865823e4f69554712e35236b9@91.227.33.18:13656"
 sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
@@ -90,6 +90,7 @@ s%:26660%:${EMPED_PORT}660%g" $HOME/.empe-chain/config/config.toml
 sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.empe-chain/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.empe-chain/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.empe-chain/config/app.toml
+
 sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0001uempe"|g' $HOME/.empe-chain/config/app.toml
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.empe-chain/config/config.toml
 sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.empe-chain/config/config.toml
@@ -111,20 +112,14 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-printGreen "8. Mengatur ulang dan mengunduh snapshot jika tersedia..." && sleep 1
-rm -rf $HOME/.empe-chain/data
-if curl -s --head  --request GET https://server-5.itrocket.net/testnet/empeiria/snapshot.tar.lz4 | head -n 1 | grep "200 OK" > /dev/null; then
-  curl -o snapshot.tar.lz4 https://server-5.itrocket.net/testnet/empeiria/snapshot.tar.lz4
-  lz4 -d snapshot.tar.lz4 | tar xf - -C $HOME/.empe-chain
-  rm snapshot.tar.lz4
-else
-  echo "Snapshot tidak ditemukan. Melanjutkan dengan sinkronisasi node..."
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+emped tendermint unsafe-reset-all --home $HOME/.empe-chain
+if curl -s --head curl https://server-5.itrocket.net/testnet/empeiria/empeiria_2024-07-31_759028_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-5.itrocket.net/testnet/empeiria/empeiria_2024-07-31_759028_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+    else
+  echo "no snapshot founded"
 fi
-sleep 1
 
 sudo systemctl daemon-reload
 sudo systemctl enable emped
-sudo systemctl start emped
-
-echo -e "${HIJAU}Node sekarang berjalan! Pantau log dengan 'sudo journalctl -u emped -f'.${NC}"
-echo -e "${HIJAU}Gunakan perintah 'emped status' untuk memeriksa status node Anda.${NC}"
+sudo systemctl restart emped && sudo journalctl -u emped -f
